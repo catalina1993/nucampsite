54d99a45aeed37ad8c06f9b6cd45b80497711ae8
Calherbe Valcin <mgenerator01@hotmail.com>
2025-01-10 13:30:34 -0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//counter and logo import removed due to course instructions
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      hello nucamp!
+    </div>
+  );
+}
+
+export default App;
