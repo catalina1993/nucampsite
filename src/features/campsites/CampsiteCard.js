08411f0dc38ead90b55e569bd9c9e09a2b2cabe9
Catalina Ispas <katy.ispas@gmail.com>
2025-01-19 11:31:10 -0500

    Added about and contact page

--- src/features/campsites/CampsiteCard.js
+++ src/features/campsites/CampsiteCard.js
@@ -1,16 +1,19 @@
 import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
+import { Link } from "react-router-dom";
 
 // Task 1 - Part 1: Create CampsiteCard.js
 
 const CampsiteCard = ({ campsite }) => {
-  const { image, name } = campsite;
+  const { id, image, name } = campsite;
   return (
-    <Card>
-      <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-      <CardImgOverlay>
-        <CardTitle>{campsite.name}</CardTitle>
-      </CardImgOverlay>
-    </Card>
+    <Link to={`${id}`}>
+      <Card>
+        <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+        <CardImgOverlay>
+          <CardTitle>{campsite.name}</CardTitle>
+        </CardImgOverlay>
+      </Card>
+    </Link>
   );
 };
 
