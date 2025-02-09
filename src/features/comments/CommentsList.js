28d5685f686b0da285ee24493747b3eb2cc4e3a6
Catalina Ispas <katy.ispas@gmail.com>
2025-02-09 14:45:46 -0500

    Comments

--- src/features/comments/CommentsList.js
+++ src/features/comments/CommentsList.js
@@ -3,25 +3,30 @@ import { Col } from "reactstrap";
 import Comment from "./Comment";
 import { selectCommentsByCampsiteId } from "./commentsSlice";
 import CommentForm from "./CommentForm";
+import Loading from "../../components/Loading";
+import Error from "../../components/Error";
 
 const CommentsList = ({ campsiteId }) => {
   const comments = useSelector(selectCommentsByCampsiteId(campsiteId));
+  const isLoading = useSelector((state) => state.comments.isLoading);
+  const errMsg = useSelector((state) => state.comments.errMsg);
 
-  if (comments && comments.length > 0) {
-    return (
-      <Col md="5" className="m-1">
-        <h4>Comments</h4>
-        {comments.map((comment) => (
-          <Comment key={comment.id} comment={comment} />
-        ))}
-        <CommentForm campsiteId={campsiteId} />
-      </Col>
-    );
+  if (isLoading) {
+    return <Loading />;
+  }
+  if (errMsg) {
+    return <Error errMsg={errMsg} />;
   }
 
   return (
     <Col md="5" className="m-1">
-      There are no comments for this campsite yet.
+      {comments.length > 0 ? (
+        comments.map((comment) => (
+          <Comment key={comment.id} comment={comment} />
+        ))
+      ) : (
+        <p>There are no comments for this campsite yet.</p>
+      )}
       <CommentForm campsiteId={campsiteId} />
     </Col>
   );
--- src/features/comments/commentsSlice.js
+++ src/features/comments/commentsSlice.js
@@ -1,8 +1,46 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { COMMENTS } from "../../app/shared/COMMENTS";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { baseUrl } from "../../app/shared/baseUrl";
+
+export const fetchComments = createAsyncThunk(
+  "comments/fetchComments",
+  async () => {
+    const response = await fetch(baseUrl + "comments");
+    if (!response.ok) {
+      return Promise.reject("Unable to fetch, status: " + response.status);
+    }
+    return await response.json();
+  }
+);
+
+export const postComment = createAsyncThunk(
+  "comments/postComment",
+  async (comment, { dispatch }) => {
+    try {
+      const response = await fetch(baseUrl + "comments", {
+        method: "POST",
+        body: JSON.stringify(comment),
+        headers: { "Content-Type": "application/json" },
+      });
+
+      if (!response.ok) {
+        return Promise.reject(
+          "Failed to post comment, status: " + response.status
+        );
+      }
+
+      const data = await response.json();
+      dispatch(addComment(data)); 
+      return data;
+    } catch (error) {
+      return Promise.reject(error.message);
+    }
+  }
+);
 
 const initialState = {
-  commentsArray: COMMENTS,
+  commentsArray: [],
+  isLoading: true,
+  errMsg: "",
 };
 
 const commentsSlice = createSlice({
@@ -10,23 +48,35 @@ const commentsSlice = createSlice({
   initialState,
   reducers: {
     addComment: (state, action) => {
-      console.log("addComment action.payload:", action.payload);
-      console.log("addComment state.commentsArray:", state.commentsArray);
-      const newComment = {
-        id: state.commentsArray.length + 1,
-        ...action.payload,
-      };
-      state.commentsArray.push(newComment);
+      state.commentsArray.push(action.payload);
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchComments.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchComments.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.errMsg = "";
+        state.commentsArray = action.payload;
+      })
+      .addCase(fetchComments.rejected, (state, action) => {
+        state.isLoading = false;
+        state.errMsg = action.error.message;
+      })
+      .addCase(postComment.rejected, (state, action) => {
+        alert(
+          "Your comment could not be posted\nError: " + action.error.message
+        );
+      });
   },
 });
 
 export const commentsReducer = commentsSlice.reducer;
-
 export const { addComment } = commentsSlice.actions;
 
-export const selectCommentsByCampsiteId = (campsiteId) => (state) => {
-  return state.comments.commentsArray.filter(
-    (comment) => comment.campsiteId === parseInt(campsiteId)
-  );
-};
+export const selectCommentsByCampsiteId = (campsiteId) => (state) =>
+  state.comments.commentsArray?.filter(
+    (comment) => comment.campsiteId === Number(campsiteId)
+  ) || [];
